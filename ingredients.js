91a9778f4986c82f0fc07ba9b8f6bee412d738ae
Ryan <ryaninbrazil@gmail.com>
2017-09-25 22:15:00 +0000

    Add ingredients exercise!

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0;
+  while (repeat < 1) {
+  console.log(ingredients);
+    repeat++;
+  }
+  
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var repeat = 0; repeat < 1; repeat++); {
+  console.log(ingredients); 
+}
+
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+ingredients.length-1 
+  for(var i = ingredients.length - 1; i >= 0; i --) {
+    console.log(ingredients[i]);
+  }
