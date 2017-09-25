4b0db7d990759065662cd8fa4474f66b444d00f6
Ryan <ryaninbrazil@gmail.com>
2017-09-25 21:25:19 +0000

    Adds looping exercises

---
+++ go-outside.js
@@ -0,0 +1,33 @@
+
+var cold = false; 
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+var temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+ } else {
+  console.log("Short sleeves are fine.");
+  }
+
+console.log("Now you're ready to go outside!");
+
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < 40 || temperature > -40) {
+  console.log("Maybe goign outside isn't a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella!");
+}
---
+++ is-citizen.js
@@ -0,0 +1,11 @@
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < 40 || temperature > -40) {
+  console.log("Maybe goign outside isn't a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella!");
+}
